2b32177786be91bab3e54ca315550c13234c8719
LeahNelson <webdevleah@gmail.com>
2021-02-05 17:43:12 -0600

    Handlebars added

--- server.js
+++ server.js
@@ -13,13 +13,17 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+var expressHbs = require("express-handlebars");
+app.engine("handlebars", expressHbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 // app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
 // Requiring our routes
-// require("./routes/html-routes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 // require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
